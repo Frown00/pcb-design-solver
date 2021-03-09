cdcff66ed650c5eebbbaf2342bb7a72501a12111
Fabian Kuriata <f.kuriata@t-bull.com>
2021-03-10 00:31:20 +0100

    feat: counting individual fitness

--- src/components/ga/GA.ts
+++ src/components/ga/GA.ts
@@ -4,8 +4,19 @@ export class GA {
   private params: any;
 
   createPopulation() {
-    const firstIndividual = new Individual().generateRandom();
-    console.log(firstIndividual);
-    firstIndividual.vizualize();
+    const population: Individual[] = [];
+    for(let i = 0; i < 10; i++) {
+      const individual = new Individual().generateRandom();
+      individual.countFitness();
+      population.push(individual);
+    }
+    let best = population[0];
+    for(let i = 1; i < population.length; i++) {
+      if(population[i].getFitness() < best.getFitness()) {
+        best = population[i];
+      }
+    }
+    console.log("FITNESS:", best.getFitness());
+    best.vizualize();
   }
 }
--- src/components/individual/util.ts
+++ src/components/individual/util.ts
@@ -1,3 +1,7 @@
 export function isSamePoint(p1: Point, p2: Point) {
   return p1.x === p2.x && p1.y === p2.y;
 }
+
+export function removePoint(array: Point[], point: Point) {
+  return array.filter(p => !isSamePoint(p, point));
+}
