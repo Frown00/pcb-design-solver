3e7d184a2add1cb0d066ef12a72930ef0d56fa8a
Fabian Kuriata <f.kuriata@t-bull.com>
2021-03-21 21:43:02 +0100

    calculating fitness and fix generating random individual

--- src/components/ga/individual/test.spec.ts
+++ src/components/ga/individual/test.spec.ts
@@ -4,6 +4,7 @@ import { IndividualController } from './controller';
 import * as mechanic from './mechanic';
 import * as util from './util';
 import { Direction } from './model';
+import { IPenalty } from '../types';
 
 describe('Mechanic', () => {
   describe('chooseDirection', () => {
@@ -34,6 +35,32 @@ describe('Mechanic', () => {
       expect(result).to.be.equal(4);
     });
   });
+
+  describe('isLinesIntersect()', () => {
+    it('should return true', () => {
+      // Given
+      const p1: Point = { x: 1, y: 1 };
+      const p2: Point = { x: 5, y: 5 };
+      const p3: Point = { x: 4, y: 5 };
+      const p4: Point = { x: 2, y: 1 };
+      // When
+      const result = mechanic.isLinesIntersect({p1, p2}, {p1: p3, p2: p4});
+      // Then
+      expect(result).to.be.equal(true);
+    });
+
+    it('should return false', () => {
+      // Given
+      const p1: Point = { x: 1, y: 0 };
+      const p2: Point = { x: 1, y: 2 };
+      const p3: Point = { x: 0, y: 2 };
+      const p4: Point = { x: 2, y: 2 };
+      // When
+      const result = mechanic.isLinesIntersect({p1, p2}, {p1: p3, p2: p4});
+      // Then
+      expect(result).to.be.equal(false);
+    });
+  });
 });
 
 describe('IndividualController', () => {
@@ -49,8 +76,13 @@ describe('IndividualController', () => {
       const conn1: Connection = [p1, p2];
       const conn2: Connection = [p3, p4];
       const connections: Connection[] = [conn1, conn2];
+      const penalty: IPenalty = {
+        intersection: 100,
+        pathLength: 1,
+        segmentCount: 2
+      }
       // When
-      const result = new IndividualController().generateRandom(width, height, connections)
+      const result = new IndividualController().generateRandom(width, height, connections, penalty);
       // Then
       util.log(result);
     });
--- src/components/ga/types.d.ts
+++ src/components/ga/types.d.ts
@@ -1,11 +1,13 @@
+export interface IPenalty {
+  intersection: number, 
+  segmentCount: number, 
+  pathLength: number 
+}
+
 export interface IGAParams {
   generations?: number, 
   populationSize?: number, 
   crossingProb?: number, 
   mutationProb?: number, 
-  penalty?: { 
-    intersection?: number, 
-    segmentCount?: number, 
-    pathLength?: number 
-  }
+  penalty?: IPenalty
 }
